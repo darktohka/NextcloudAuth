b30aa5ca6a340f1848fb630dcfdfeb82e387f528
Tohka <daniel@tohka.us>
2020-02-10 10:43:05 +0200

    oauth: Migrate to Requests OAuth library instead of Flask

--- nextcloudauth/app.py
+++ nextcloudauth/app.py
@@ -1,38 +1,31 @@
-
-from requests-oauthlib import OAuth
-from flask import Flask, request, jsonify, session, abort
-import os, sys, uuid, json
+from requests_oauthlib import OAuth2Session
+from flask import Flask, request, jsonify, session, abort, redirect
+import os, json
 
 class Application(object):
 
-    def __init__(self, oauth, name, settings):
-        self.oauth = oauth
+    def __init__(self, name, settings):
         self.name = name
-        self.base_url = settings['base_url']
         self.access_token_url = settings['access_token_url']
         self.authorize_url = settings['authorize_url']
 
         self.client_id = settings['client_id']
         self.client_secret = settings['client_secret']
-        self.extra = settings.get('extra', {})
+        self.scopes = settings.get('scopes', [])
         self.redirect_url = settings['redirect_url']
-
-        self.app = None
 
     def get_cookie(self, cookie):
         return '{0}_{1}'.format(self.name, cookie)
 
-    def get(self):
-        if not self.app:
-            self.app = self.oauth.remote_app(self.name,
-                access_token_url=self.access_token_url, authorize_url=self.authorize_url,
-                consumer_key=self.client_id, consumer_secret=self.client_secret,
-                request_token_params=self.extra,
-                request_token_url=None, access_token_method='POST',
-                base_url=self.base_url
-            )
+    def get_authorization_url(self):
+        session = OAuth2Session(self.client_id, redirect_uri=self.redirect_url, scope=self.scopes)
+        return session.authorization_url(self.authorize_url)
 
-        return self.app
+    def fetch_token(self, state, url):
+        url = url.replace('http://', 'https://')
+        session = OAuth2Session(client_id=self.client_id, state=state, redirect_uri=self.redirect_url)
+
+        return session.fetch_token(self.access_token_url, client_secret=self.client_secret, authorization_response=url)
 
 environ_path = os.path.join('..', 'settings.json')
 
@@ -40,13 +33,12 @@ with open(environ_path, 'r') as f:
     settings = json.load(f)
 
 app = Flask(__name__)
-app.secret_key = 'hahahaha'
+app.secret_key = settings['secret_key']
 
-oauth = OAuth(app,)
 APPS = {}
 
 for app_name, app_config in settings['applications'].items():
-    APPS[app_name] = Application(oauth, app_name, app_config)
+    APPS[app_name] = Application(app_name, app_config)
 
 def get_application(app_name):
     return APPS.get(app_name)
@@ -58,9 +50,9 @@ def oauth_request(name):
     if not app:
         abort(404, 'Unknown application.')
 
-    state = app.get_cookie('state')
-    session[state] = str(uuid.uuid4())
-    return app.get().authorize(callback=app.redirect_url, state=session[state])
+    authorization_url, state = app.get_authorization_url()
+    session[app.get_cookie('state')] = state
+    return redirect(authorization_url)
 
 @app.route('/<name>/authorized')
 def authorized(name):
@@ -69,12 +61,15 @@ def authorized(name):
     if not app:
         abort(404, 'Unknown application.')
 
-    state = app.get_cookie('state')
+    if 'state' not in request.args:
+        abort(403, 'Invalid authorization request.')
 
-    if 'state' not in request.args or str(session.pop(state, None)) != str(request.args['state']):
+    state = str(request.args['state'])
+
+    if session.pop(app.get_cookie('state'), None) != state:
         abort(403, 'No authorization request is currently in progress.')
 
-    return jsonify(app.get().authorized_response())
+    return jsonify(app.fetch_token(state, request.url))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
